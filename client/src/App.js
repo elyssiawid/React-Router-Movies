9e9c80b01804ad4885ad48a79e5be4108ee2ff8f
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-07-22 15:38:20 -0700

    fixed errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
-import { Movie, MovieList, MovieCard } from "./Movies";
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 
 import SavedList from "./Movies/SavedList";
 
